91d278aa71430c0501805b68482f6c3c74beae6c
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2024-12-30 22:33:33 +0900

    Drawing example 2 - human
    - `arc`를 이용하여 원을 그릴 수 있다.
      - 다양한 argument가 있으니 공식 문서를 보면 좋다.
      - https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
      - `arc`를 그릴 때는그림이 시작하는 위치로 점을 옮긴 후에 그린다. (`ctx.moveTo(x+r, y)` 후 그리기)

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -4,20 +4,37 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-/* wall */
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
+/* arms */
+ctx.fillRect(200, 200, 20, 100);
+ctx.fillRect(340, 200, 20, 100);
 
-/* door */
-ctx.lineWidth = 3;
-ctx.strokeRect(300, 300, 50, 100);
+/* body */
+ctx.fillRect(250, 200, 60, 150);
 
-/* celling */
-ctx.fillRect(200, 200, 250, 30);
-
-/* roof */
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.lineTo(200, 200);
+/* head */
+ctx.moveTo(320, 150);
+ctx.arc(280, 150, 40, 0, Math.PI * 2);
 ctx.fill();
+
+/* eyes */
+ctx.beginPath();
+ctx.moveTo(268, 140);
+ctx.arc(260, 140, 8, 0, Math.PI * 2);
+ctx.moveTo(308, 140);
+ctx.arc(300, 140, 8, 0, Math.PI * 2);
+ctx.fillStyle = "yellow";
+ctx.fill();
+
+/* mouth */
+ctx.beginPath();
+ctx.moveTo(290, 160);
+ctx.arc(280, 160, 10, 0, Math.PI);
+ctx.strokeStyle = "white";
+ctx.lineWidth = 5;
+ctx.stroke();
+
+/* legs */
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(250, 350, 20, 100);
+ctx.fillRect(290, 350, 20, 100);
