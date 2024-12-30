86549ab34f1c0576e9338ea3246d5460ca4fa83a
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2024-12-30 22:08:13 +0900

    Drawing example 1 - house

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -4,9 +4,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+/* wall */
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+
+/* door */
+ctx.lineWidth = 3;
+ctx.strokeRect(300, 300, 50, 100);
+
+/* celling */
+ctx.fillRect(200, 200, 250, 30);
+
+/* roof */
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.lineTo(200, 200);
 ctx.fill();
