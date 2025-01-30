a9f78136589562a905fb270762d1b3f6498d9266
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2025-01-30 18:17:02 +0900

    Draw lines
    - 마우스를 클릭한 상태를 나타내는 event는 `mousedown`, 이 상태에서 클릭을 뗀 상태를 나타내는 event는 `mouseup`
    - 커서가 구역을 벗어나는 것을 나타내는 event는 `mouseleave`

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,40 +1,30 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
 canvas.width = 800;
 canvas.height = 800;
 
-/* arms */
-ctx.fillRect(200, 200, 20, 100);
-ctx.fillRect(340, 200, 20, 100);
+ctx.lineWidth = 2;
 
-/* body */
-ctx.fillRect(250, 200, 60, 150);
+let isDrawing = false;
 
-/* head */
-ctx.moveTo(320, 150);
-ctx.arc(280, 150, 40, 0, Math.PI * 2);
-ctx.fill();
+function drawingLine(event) {
+  if (isDrawing) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
 
-/* eyes */
-ctx.beginPath();
-ctx.moveTo(268, 140);
-ctx.arc(260, 140, 8, 0, Math.PI * 2);
-ctx.moveTo(308, 140);
-ctx.arc(300, 140, 8, 0, Math.PI * 2);
-ctx.fillStyle = "yellow";
-ctx.fill();
+function startDrawing() {
+  isDrawing = true;
+}
 
-/* mouth */
-ctx.beginPath();
-ctx.moveTo(290, 160);
-ctx.arc(280, 160, 10, 0, Math.PI);
-ctx.strokeStyle = "white";
-ctx.lineWidth = 5;
-ctx.stroke();
+function stopDrawing() {
+  isDrawing = false;
+}
 
-/* legs */
-ctx.beginPath();
-ctx.fillStyle = "black";
-ctx.fillRect(250, 350, 20, 100);
-ctx.fillRect(290, 350, 20, 100);
+canvas.addEventListener("mousemove", drawingLine);
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseleave", stopDrawing);
