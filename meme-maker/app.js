48e49a99304fe8667060a5fe81d6e08284307c04
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2025-01-30 19:36:31 +0900

    Add color selector
    두 가지 방법으로 색상을 선택하게 함
    1. `input type = "color"`을 통해 색상 선택 기능 구현
      - 이 방법으로 하면 유저가 색상을 RGB로 입력해야 하는 불편함이 있다.
    2. 색상 팔레트에서 원하는 색 선택
      - HTML에서 `data-` attribute를 사용하면 `event.target.dataset`에서 해당 속성의 key를 가지게 됨
      - `data-color`을 추가하면 `event.target.dataset.color`에서 값을 가져올 수 있음

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,9 +1,14 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
+const colorOptions = document.querySelectorAll(".color-option");
+
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
+ctx.strokeStyle = color.value;
+ctx.fillStyle = color.value;
 
 let isDrawing = false;
 
@@ -29,9 +34,27 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function colorChange(colorValue) {
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onColorChange(event) {
+  colorChange(event.target.value);
+}
+
+function onColorOptionClick(event) {
+  colorChange(event.target.dataset.color);
+}
+
 canvas.addEventListener("mousemove", drawingLine);
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", stopDrawing);
 canvas.addEventListener("mouseleave", stopDrawing);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((colorOption) => {
+  colorOption.addEventListener("click", onColorOptionClick);
+});
